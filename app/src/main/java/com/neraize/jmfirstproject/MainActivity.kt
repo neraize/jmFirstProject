f00d3003ffaa39012378137f735c398469659c10
seojungmin <neraize@gmail.com>
2022-03-18 10:20:58 +0900

    #1 깃허브테스트완료 -메인작업시작 시작점

--- app/src/main/java/com/neraize/jmfirstproject/MainActivity.kt
+++ app/src/main/java/com/neraize/jmfirstproject/MainActivity.kt
@@ -2,10 +2,17 @@ package com.neraize.jmfirstproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neraize.jmfirstproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
     }
 }
