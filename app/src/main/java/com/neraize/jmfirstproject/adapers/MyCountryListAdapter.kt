d4abca573e9c6251bf9ff33392b27f65e0c490dc
seojungmin <neraize@gmail.com>
2022-03-25 18:12:27 +0900

    #4 메인화면 그리기(파트별화면-알람삭제오류!) -MyCountryListAdapter: 프래그먼트 단에서 버튼 클릭시,, 알람 추가는 잘되나,   알람 삭제시 오류 발생
    * my_country_list_item:  btnSetAlarm버튼에 태그 달아봄

--- app/src/main/java/com/neraize/jmfirstproject/adapers/MyCountryListAdapter.kt
+++ app/src/main/java/com/neraize/jmfirstproject/adapers/MyCountryListAdapter.kt
@@ -12,6 +12,7 @@ import android.widget.TextView
 import androidx.core.content.ContextCompat
 import com.neraize.jmfirstproject.MainActivity
 import com.neraize.jmfirstproject.R
+import com.neraize.jmfirstproject.api.FirebaseDbConnect
 import com.neraize.jmfirstproject.datas.CountryData
 
 class MyCountryListAdapter(
@@ -19,6 +20,8 @@ class MyCountryListAdapter(
     val resId:Int,
     val mList:ArrayList<CountryData>
 ):ArrayAdapter<CountryData>(mContext, resId, mList) {
+
+    var isAlarmSet =false
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
 
@@ -44,37 +47,48 @@ class MyCountryListAdapter(
         }
 
         // 기존 나의 알림리스트 내역 확인하여 화면에 뿌려주기
-        Log.d("MyCountryListAdapter사이즈", "${MainActivity.mAlarmList.size.toString()}")
+        //Log.d("MyCountryListAdapter사이즈", "${MainActivity.mAlarmList.size.toString()}")
 
         MainActivity.mAlarmList.forEach { alarm->
             if(data.name == alarm.pushCountry){
-                Log.d("MyCountryListAdapter", "알림OK")
+                //Log.d("MyCountryListAdapter", "알림OK")
+                isAlarmSet = true
                 btnSetAlarm.text ="알림OK"
+                btnSetAlarm.tag="true"
                 btnSetAlarm.setBackgroundColor(ContextCompat.getColor(mContext,R.color.black_bottom))
                 return@forEach
             }
     }
 
-
-
         // 알림신청 버튼클릭 이벤트
         btnSetAlarm.setOnClickListener {
 
-            if(btnSetAlarm.text == "알림신청"){
+            Log.d("MyCountryListAdapter1","알람 ${isAlarmSet}")
+
+
+            FirebaseDbConnect.setMyAlarmList(MainActivity.mUserIdReplaceDotToStar, data.name, !isAlarmSet)
+            isAlarmSet =!isAlarmSet
+
+            if(btnSetAlarm.tag == "false"){
                 btnSetAlarm.setBackgroundColor(ContextCompat.getColor(mContext,R.color.black_bottom))
+                isAlarmSet=true
                 btnSetAlarm.text ="알림OK"
+                btnSetAlarm.tag ="true"
+                Log.d("MyCountryListAdapter1","알람신청ok")
             }
             else{
-
+                isAlarmSet=false
+                btnSetAlarm.text ="알림신청"
+                btnSetAlarm.tag ="flase"
                 when(data.possibility){
                     "여행가능"-> btnSetAlarm.setBackgroundColor(ContextCompat.getColor(mContext,R.color.blue_bottom))
                     "국내격리"-> btnSetAlarm.setBackgroundColor(ContextCompat.getColor(mContext,R.color.green_bottom))
                     "국내/국외격리"-> btnSetAlarm.setBackgroundColor(ContextCompat.getColor(mContext,R.color.yellow2_bottom))
                     "입국금지"-> btnSetAlarm.setBackgroundColor(ContextCompat.getColor(mContext,R.color.red_bottom))
                 }
-
-                btnSetAlarm.text ="알림신청"
+                Log.d("MyCountryListAdapter1","알람취소ok")
             }
+            Log.d("MyCountryListAdapter2","알람 ${isAlarmSet}")
         }
         return  row
     }
