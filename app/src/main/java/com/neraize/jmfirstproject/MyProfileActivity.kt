18e6074706021b0a133c2e52ca4e1224dfc0280e
neraize <neraize@gmail.com>
2022-03-27 21:55:13 +0900

    #14 나의정보 (기초세팅) -리스트뷰기초작업 1) activity_my_profile.xml: ListView태그 배치 2) 보여질 모양의 xml그리기 3) 데이터표현용 클래스(kt)기존꺼 사용 4) 어댑터 제작 및 연결

--- app/src/main/java/com/neraize/jmfirstproject/MyProfileActivity.kt
+++ app/src/main/java/com/neraize/jmfirstproject/MyProfileActivity.kt
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.databinding.DataBindingUtil
+import com.neraize.jmfirstproject.adapers.MyAlarmListAdapter
 import com.neraize.jmfirstproject.databinding.ActivityMyProfileBinding
 
 class MyProfileActivity : BaseActivity() {
 
     lateinit var binding:ActivityMyProfileBinding
+
+    lateinit var mAdapter:MyAlarmListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,5 +29,11 @@ class MyProfileActivity : BaseActivity() {
 
         imgHome.visibility = View.VISIBLE
         txtProfile.visibility =  View.GONE
+
+        mAdapter = MyAlarmListAdapter(mContextt, R.layout.my_alarm_list_item, MainActivity.mAlarmList)
+        binding.myAlarmListView.adapter = mAdapter
+
+        // 어댑터 새로고침
+        //mAdapter.notifyDataSetChanged()
     }
 }
---
+++ app/src/main/java/com/neraize/jmfirstproject/adapers/MyAlarmListAdapter.kt
@@ -0,0 +1,103 @@
+package com.neraize.jmfirstproject.adapers
+
+import android.content.Context
+import android.content.res.Resources
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.neraize.jmfirstproject.MainActivity
+import com.neraize.jmfirstproject.R
+import com.neraize.jmfirstproject.api.FirebaseDbConnect
+import com.neraize.jmfirstproject.datas.CountryData
+import com.neraize.jmfirstproject.datas.MyAlarmData
+
+class MyAlarmListAdapter(
+    val mContext:Context,
+    val resId:Int,
+    val mList:ArrayList<MyAlarmData>
+):ArrayAdapter<MyAlarmData>(mContext, resId, mList) {
+
+    var isAlarmSet =false
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow =  convertView
+        if(tempRow == null){
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.my_alarm_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        val data =mList[position]
+
+        val txtName = row.findViewById<TextView>(R.id.txtTitle)
+        val txtPossibility = row.findViewById<TextView>(R.id.txtPossibility)
+        val btnSetAlarm = row.findViewById<Button>(R.id.btnSetAlarm)
+
+        txtName.text = data.pushCountry
+//        txtPossibility.text =data.possibility
+
+//        when(data.possibility){
+//            "여행가능"-> btnSetAlarm.setBackgroundColor(ContextCompat.getColor(mContext,R.color.blue_bottom))
+//            "국내격리"-> btnSetAlarm.setBackgroundColor(ContextCompat.getColor(mContext,R.color.green_bottom))
+//            "국내/국외격리"-> btnSetAlarm.setBackgroundColor(ContextCompat.getColor(mContext,R.color.yellow2_bottom))
+//            "입국금지"-> btnSetAlarm.setBackgroundColor(ContextCompat.getColor(mContext,R.color.red_bottom))
+//        }
+
+        // 기존 나의 알림리스트 내역 확인하여 화면에 뿌려주기
+        //Log.d("MyCountryListAdapter사이즈", "${MainActivity.mAlarmList.size.toString()}")
+
+//        MainActivity.mAlarmList.forEach { alarm->
+//            if(data.name == alarm.pushCountry){
+//                //Log.d("MyCountryListAdapter", "알림OK")
+//                isAlarmSet = true
+//                btnSetAlarm.text ="알림ON"
+//                btnSetAlarm.tag="true"
+//                btnSetAlarm.setBackgroundColor(ContextCompat.getColor(mContext,R.color.black_bottom))
+//                return@forEach
+//            }
+//        }
+
+        // 알림신청 버튼클릭 이벤트
+//        btnSetAlarm.setOnClickListener {
+//
+//            Log.d("MyCountryListAdapter","알람클릭전 ${btnSetAlarm.tag}")
+//            if(btnSetAlarm.tag =="true"){
+//                isAlarmSet=true
+//            }
+//            else{
+//                isAlarmSet=false
+//            }
+//
+//            FirebaseDbConnect.setMyAlarmList(MainActivity.mUserIdReplaceDotToStar, data.name, isAlarmSet)
+////            isAlarmSet =!isAlarmSet
+//
+//            if(btnSetAlarm.tag =="false"){
+//                btnSetAlarm.setBackgroundColor(ContextCompat.getColor(mContext,R.color.black_bottom))
+//                isAlarmSet=true
+//                btnSetAlarm.text ="알림ON"
+//                btnSetAlarm.tag ="true"
+//                Log.d("MyCountryListAdapter1","알림ON")
+//            }
+//            else{
+//                isAlarmSet=false
+//                btnSetAlarm.text ="알림OFF"
+//                btnSetAlarm.tag ="false"
+//                when(data.possibility){
+//                    "여행가능"-> btnSetAlarm.setBackgroundColor(ContextCompat.getColor(mContext,R.color.blue_bottom))
+//                    "국내격리"-> btnSetAlarm.setBackgroundColor(ContextCompat.getColor(mContext,R.color.green_bottom))
+//                    "국내/국외격리"-> btnSetAlarm.setBackgroundColor(ContextCompat.getColor(mContext,R.color.yellow2_bottom))
+//                    "입국금지"-> btnSetAlarm.setBackgroundColor(ContextCompat.getColor(mContext,R.color.red_bottom))
+//                }
+//                Log.d("MyCountryListAdapter","알림OFF")
+//            }
+//            Log.d("MyCountryListAdapter","알람클릭후 ${btnSetAlarm.tag}")
+//        }
+        return  row
+    }
+}
