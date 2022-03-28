26f6c9b6be3b1190667cd9c437a2984f077368ba
seojungmin <neraize@gmail.com>
2022-03-28 16:52:57 +0900

    #16 관리자페이지  -Spinner기본사용 1)activity_administrator.xml: "Spinner"태그 삽입 2)array.xml : 표현할 아이템항목 그리기 3)AdministratorActivity : Spinner기본어댑터 연결 & 선택시마다 표현할 값세팅

--- app/src/main/java/com/neraize/jmfirstproject/AdministratorActivity.kt
+++ app/src/main/java/com/neraize/jmfirstproject/AdministratorActivity.kt
@@ -3,16 +3,34 @@ package com.neraize.jmfirstproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.neraize.jmfirstproject.databinding.ActivityAdministratorBinding
 
-class AdministratorActivity : BaseActivity() {
+class AdministratorActivity : BaseActivity(), AdapterView.OnItemSelectedListener {
 
     lateinit var binding:ActivityAdministratorBinding
+
+    lateinit var spnPossibility:String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_administrator)
+
+        // spinner 연결
+        ArrayAdapter.createFromResource(
+            mContextt,
+            R.array.my_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            binding.spinner.adapter = adapter
+        }
+        binding.spinner.onItemSelectedListener = this
 
         SetupEvents()
         SetValues()
@@ -31,5 +49,20 @@ class AdministratorActivity : BaseActivity() {
         txtProfileOut.setOnClickListener {
             finish()
         }
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        when(position){
+            0-> spnPossibility ="여행가능"
+            1-> spnPossibility ="국내격리"
+            2-> spnPossibility ="국내/국외격리"
+            else-> spnPossibility ="국내/입국금지"
+        }
+
+        //Toast.makeText(mContextt, spnPossibility, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+
     }
 }
